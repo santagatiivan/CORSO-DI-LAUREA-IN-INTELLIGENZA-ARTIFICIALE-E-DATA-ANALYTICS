8a2ffaea6f304b90ba6d2c72dde7b339d3099c73
santagatiivan <ivanrepnic15@gmail.com>
2024-10-30 01:09:56 +0100

    sistemazione esercizi

--- primo_anno/primo_semestre/INTRODUZIONE_ALLA_PROGRAMMAZIONE_E_LABORATORIO/prove_esame/1_parziale/2022/2/main.c
+++ primo_anno/primo_semestre/INTRODUZIONE_ALLA_PROGRAMMAZIONE_E_LABORATORIO/prove_esame/1_parziale/2022/2/main.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
-#include <math.h>
 
-int primo (int x){
+int primo (int k, int divisore,  int divisore_max){
+    if (k<2) return (0);
+    if (divisore > divisore_max) return (1);
+    if (k % divisore == 0) return (0);
 
-    if (x < 2) {
-        return 0;
-    }
-
-    for (int i = 2; i <= sqrt(x); i++) {
-        if (x % i == 0) {
-            return 0; 
-        }
-    }
-
-    return 1;
+    return (primo(k, divisore + 1, divisore_max));
 }
 
-
-int ricorsiva (){
-
+int calcola_pi_k_n(int k, int n){
+    if (k > n) return 0;
+    return k * primo(k, 2, k/2) + calcola_pi_k_n(k + 1, n);
 }
 
-int main (){
-
-int x;
-
-printf("Inserire x: ");
-scanf("%d", &x); 
+int main(){
+    int result = calcola_pi_k_n(3, 21);
+    printf("Il valore di π_{3,21} è: %d\n", result);
 
     return 0;
 }
